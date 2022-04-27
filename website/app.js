fd7de2826233f6528ac37a3c69c862c5f3b25276
FBOELKE <fboelke@emea.corpdir.net>
2022-04-27 08:54:40 +0200

    Add WebAPI

--- website/app.js
+++ website/app.js
@@ -1,5 +1,9 @@
 /* Global Variables */
+// BaseURL for 'Current weather data' from OpenWeatherMap
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?';
+// Personal API Key for OpenWeatherMap API
+const apiKey = '273ac0cab962762527c58f48b36712ff&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
