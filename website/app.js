5def511ef7a6bc5b666db3e4cca05add9ff42f55
FBOELKE <fboelke@emea.corpdir.net>
2022-04-28 20:03:15 +0200

    Corretion of Month (Date)

--- website/app.js
+++ website/app.js
@@ -6,7 +6,7 @@ const apiKey = '273ac0cab962762527c58f48b36712ff&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
 
 // Event listener to add function to existing HTML DOM element
 document.querySelector('#generate').addEventListener('click', performAction);
